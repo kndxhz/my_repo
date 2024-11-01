e762b2a0fd6da6a4db268596e2592f8c35949356
kndxhz <kndxhz@163.com>
2024-11-01 23:25:17 +0800

    feat:全部格式化

--- 3in1/3in1.py
+++ 3in1/3in1.py
@@ -2,19 +2,20 @@ from flask import Flask, request, redirect, render_template_string, send_file
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def check_ua():
-    user_agent = request.headers.get('User-Agent', '').lower()
+    user_agent = request.headers.get("User-Agent", "").lower()
     print(user_agent)
-    if 'micromessenger' in user_agent:
-        return send_file('wechat.png', mimetype='image/jpeg')
-    elif 'alipayclient' in user_agent:
+    if "micromessenger" in user_agent:
+        return send_file("wechat.png", mimetype="image/jpeg")
+    elif "alipayclient" in user_agent:
         return redirect("https://qr.alipay.com/fkx19092rwrfit8qoafzl50")
-    elif 'qqtheme' in user_agent:
-        return send_file('QQ.png', mimetype='image/jpeg')
-        #return redirect("mqqopensdkapi://bizAgent/qm/qr?url=https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&ac=CAEQi6616AIYgJnYowZCIDY0NDRiYzRkNTUwNWQzZGVhNjY4ZWMxNTNmZjJiYmE3_xxx_sign&u=755848971&n=%E5%8F%AF%E8%80%90%E7%9A%84%E5%B0%8F%E4%BC%99%E7%BA%B8")
+    elif "qqtheme" in user_agent:
+        return send_file("QQ.png", mimetype="image/jpeg")
+        # return redirect("mqqopensdkapi://bizAgent/qm/qr?url=https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&ac=CAEQi6616AIYgJnYowZCIDY0NDRiYzRkNTUwNWQzZGVhNjY4ZWMxNTNmZjJiYmE3_xxx_sign&u=755848971&n=%E5%8F%AF%E8%80%90%E7%9A%84%E5%B0%8F%E4%BC%99%E7%BA%B8")
     else:
-        html_content = f'''
+        html_content = f"""
         <html>
             <head>
                 <title>扫码提示</title>
@@ -41,8 +42,9 @@ def check_ua():
                 </div>
             </body>
         </html>
-        '''
+        """
         return render_template_string(html_content)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=677)
--- first_name/first_name.py
+++ first_name/first_name.py
@@ -1,39 +1,40 @@
 import asyncio
 from pypinyin import pinyin
 
+
 async def get_initials(chinese_str):
     initials = [word[0][0].upper() for word in pinyin(chinese_str)]
-    return ''.join(initials)
+    return "".join(initials)
+
 
 async def main():
     try:
-        with open('names.txt', 'r', encoding='utf-8') as f:
+        with open("names.txt", "r", encoding="utf-8") as f:
             names = [line.strip() for line in f.readlines()]
     except FileNotFoundError:
         print("names.txt文件未找到")
         return
-    
 
-    
     # 创建首字母到名字的映射
     initials_dict = {}
     initials = await asyncio.gather(*[get_initials(name.strip()) for name in names])
-    
+
     for idx, initial in enumerate(initials):
         if initial not in initials_dict:
             initials_dict[initial] = []
         initials_dict[initial].append(names[idx])
-    
-    print("对应字典:",initials_dict)
-    
+
+    print("对应字典:", initials_dict)
+
     name = input("输入缩写: ")
     name = name.upper()
-    
+
     # 查找对应的名字
     if name in initials_dict:
         print("找到的名字:", initials_dict[name])
     else:
         print("未找到")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
